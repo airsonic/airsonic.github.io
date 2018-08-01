c7538d3cb8404e79c08f03412f6c621534b7661b
jo <lammler.jonas@riseup.net>
2018-08-01 15:58:36 +0200

    Revert moving to src folder
    Github doesn't allow to use a subfolder to keep the site sources...
    https://help.github.com/articles/configuring-jekyll/

--- update_submodule.sh
+++ update_submodule.sh
@@ -2,12 +2,12 @@
 
 # Be sure to run this script at the airsonic/airsonic.github.io root folder and that your submodule are cloned locally.
 
-pushd src/pages/docs
+pushd pages/docs
 git fetch
 git checkout master
 git pull
 popd
-git add src/pages/docs
+git add pages/docs
 git commit -m 'Update docs submodule'
 
 # EOF
